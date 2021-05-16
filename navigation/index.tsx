c66530c849b9e282e3f76b784bdbf663cbfbedf6
Prince Alfred Gyan <princealfredgyan@gmail.com>
2021-05-16 17:45:36 +0100

    Status Bar -Stack bar- Search Icon - three vertical

---
+++ navigation/index.tsx
@@ -0,0 +1,67 @@
+/**
+ * If you are not familiar with React Navigation, check out the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+ import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+ import { createStackNavigator } from '@react-navigation/stack';
+ import * as React from 'react';
+ import { ColorSchemeName } from 'react-native';
+ //import { Colors } from 'react-native/Libraries/NewAppScreen';
+ 
+ import NotFoundScreen from '../screens/NotFoundScreen';
+ import { RootStackParamList } from '../types';
+ import BottomTabNavigator from './BottomTabNavigator';
+ import LinkingConfiguration from './LinkingConfiguration';
+ import Colors from '../constants/Colors';
+ import { Ionicons, MaterialCommunityIcons, MaterialIcons, Octicons } from '@expo/vector-icons';
+ import { View } from '../components/Themed';
+ 
+ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+   return (
+     <NavigationContainer
+       linking={LinkingConfiguration}
+       theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+       <RootNavigator />
+     </NavigationContainer>
+   );
+ }
+ 
+ // A root stack navigator is often used for displaying modals on top of all other content
+ // Read more here: https://reactnavigation.org/docs/modal
+ const Stack = createStackNavigator<RootStackParamList>();
+ 
+ function RootNavigator() {
+   return (
+     <Stack.Navigator screenOptions={{ 
+       headerStyle:{
+         backgroundColor: Colors.light.tint,
+       },
+       headerTintColor: Colors.light.background,
+       headerTitleAlign: 'left',
+       headerTitleStyle:{
+         fontWeight:'bold',
+       }
+     }}>
+       <Stack.Screen name="Root" component={BottomTabNavigator}
+       options={{
+         title:'ThatApp',
+         headerRight:() =>(
+           <View style={{
+             flexDirection:'row',
+             backgroundColor: Colors.light.tint,
+             width:60,
+             justifyContent:'space-between',
+             marginRight:10,
+           }}>
+             <MaterialIcons name="search" size={25} color="white" />
+             <MaterialCommunityIcons name='dots-vertical' size={25} color="white" />
+           </View>
+ 
+         )
+       }} />
+       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+     </Stack.Navigator>
+   );
+ }
+ 
